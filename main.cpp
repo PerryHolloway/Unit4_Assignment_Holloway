d659fa2deb4ce9d23a1696a04e40c39e7c9b6a52
Perry Holloway <perry.holloway@my.tccd.edu>
2019-11-20 23:31:36 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+//Perry Holloway
+//Dr.T
+//Fundamental Comp II
+//November 6,2019
+#include <iostream>
+#include <vector>
+#include<string>
+#include<algorithm>
+#include<numeric>
+using namespace std;
+
+void gradesVector(vector<double> &vec,int size)
+{
+  double input=0.0;
+ for(int i=0;i<size;i++)
+ {
+   cout<<"Please enter " << (i +1) <<" "<< size<<": ";
+   cin >> input;
+  vec.push_back(input);
+ }
+}
+
+
+int main() {
+  vector<double> grades;
+  //information before averaging
+  //input from user
+  gradesVector(grades,10);
+ 
+  double average = accumulate( grades.begin(), grades.end(), 0.0/
+  grades.size());
+  cout << "The average is" <<" " << average << endl;
+
+
+  sort(grades.begin(), grades.end());
+  reverse(grades.begin(), grades.end());
+  for (double d : grades) {
+    cout << d << endl;
+}
+
+return 0;
+}
